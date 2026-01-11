bbc21ea94e110c1f5959474d25fdc86c2442c95f
aperumal <anandperumal82@gmail.com>
2026-01-10 21:48:56 -0500

    Initial commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LogManager.getLogger(HelloController.class);
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, Spring!";
+        logger.info("Received request to / endpoint");
+        logger.debug("Processing hello request");
+        String response = "Hello, Spring!";
+        logger.info("Returning response: {}", response);
+        return response;
     }
 }
